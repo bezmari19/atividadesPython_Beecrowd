6fe612d2f2aec6b655fced5362b66428997006bf
Marina Bezerra <156716308+bezmari19@users.noreply.github.com>
2025-02-07 19:29:42 -0300

    Exercício1

---
+++ exercíciosResolvidos/PrimeiroHelloWorld.py
@@ -0,0 +1,8 @@
+#O primeiro exercício e o mais básico de todos, o seu primeiro "Olá mundo!"
+
+entrada = 'Hello World!' #Nesse caso, você não precisará colocar o input, já que sua variável já está previamente definida.
+
+#lembre-se que ao dar nomes às suas váriaveis é preferivel que seja algo mais fácil de entender, que seja =(igual) ao nome que foi dado no caso é a entrada da frase então: váriavel = entrada(nome). Certo?
+
+#também é bom mencionar, caso você não esteja acostumado - AINDA - use sempre letras minusculas, caso sua variavel for um nome mais "longo" faça uso de caracteres como _(underline) ou letra maiúscula no segundo, exemplo: "entradaFrase" já que o programa não aceitara espaço e dessa forma ocorrerá um erro.
+print(entrada) #O print é o que você quer imprimir no seu programa, ou seja, a primeira frase na vida de um desenvolvedor! A variavel "entrada" que é = a frase aparecerá como resposta.
