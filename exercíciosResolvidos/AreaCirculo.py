45d1b6317dd81f1ddd92051ab5067e7d2fed27c0
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-15 18:13:38 -0300

    commit feito

---
+++ exercíciosResolvidos/AreaCirculo.py
@@ -0,0 +1,4 @@
+#O número do raio com casas decimais a mais. Float(casas decimais) = valor a ser atribuido na entrada 
+raio = float(input()) 
+n = 3.14159 #O número que multiplicada pelo raio da o resultado da área. O número ao qual a váriavel N foi atribuida 
+print(f'A={n * raio**2:.4f}') #Resultado com a conta feita para achar áreas de circulos, com 4 casas decimais a mais imprimindo. O saida.
