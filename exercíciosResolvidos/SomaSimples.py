97968171e28f7ac71555ba95dcd4074b3c9c6142
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-15 18:18:20 -0300

    Commit feito

---
+++ exercíciosResolvidos/SomaSimples.py
@@ -0,0 +1,8 @@
+#Soma dos dois valores INTEIROS(int) atribuídos na entrada
+valor1 = int(input())
+valor2 = int(input())
+#Condições das somas que devem ser imprimidas no Output 
+if valor1 == -valor1: #Se for ==(comparativamente) inteiro/menor que o Valor1 (primeira condição)
+  print('SOMA', '=', valor1 + valor2)
+else: #Else (qualquer outra CONDIÇÃO que NÃO seja IGUAL a primeira, VERDADEIRA)
+  print('SOMA', '=', valor1 + valor2) #Imprimir resultado como pedido 
