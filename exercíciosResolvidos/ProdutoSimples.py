cd2371009c592528db9fdca3f593264d0e383121
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-15 18:22:47 -0300

    Commit feito

---
+++ exercíciosResolvidos/ProdutoSimples.py
@@ -0,0 +1,9 @@
+#Inteiros(int) a serem atribuidos a entrada
+produto1 = int(input())
+produto2 = int(input())
+#Condições as quais os produtos devem ser cálculados
+if produto1 == -produto1: #Se for ==(comparativamente) inteiro/menor que o Valor1 (primeira condição)
+  print('PROD', '=', produto1 * produto2 )
+else: #Else (qualquer outra CONDIÇÃO que NÃO seja IGUAL a primeira, VERDADEIRA)
+  print('PROD', '=', produto1 * produto2 )
+  #O valor impresso "produto" é == à multiplicação em operações matemáticas - normalmente 
