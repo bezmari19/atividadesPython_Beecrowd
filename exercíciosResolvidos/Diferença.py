7d4ae6a8a9d57ae141e80ce9c93fc43b1a6d4b3a
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-25 19:35:56 -0300

    commit feito

---
+++ exercíciosResolvidos/Diferença.py
@@ -0,0 +1,9 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+#Preencher as entradas das variáveis com números inteiros
+diferenca = A * B - C * D
+#Criar uma string que atenda ao calculo pedido no exemplo, onde irá imprimir o resultado dessa diferença
+print('DIFERENCA =', diferenca)
+#print conforme indicado no exemplo de Output() com a string 'DIFERENÇA =' em CapsLock e a função de string ',' como uma boa prática de código na sintaxe para separar a string da variável 
