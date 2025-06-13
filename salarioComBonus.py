5305bd3ae887394d176497579c49cd2515f02e42
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-13 18:30:29 -0300

    fix: docs: adicionando exercício à pasta

--- salarioComBonus.py
+++
@@ -1,8 +0,0 @@
-nomeVendedor = input()
-salarioFixo = float(input())
-totalVendas = float(input())
-
-comissaoDada = totalVendas * 0.15
-totalMes = salarioFixo + comissaoDada
-
-print(f'TOTAL = R$ {totalMes:.2f}')
